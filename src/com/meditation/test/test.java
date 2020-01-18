3a89f659b4ec4c5e1d6341180862f228adb708d8
T-engo <1092738496@qq.com>
2020-01-18 14:38:12 +0800

    1.18 Idea 与 git远程推送连接成功!

--- src/com/meditation/test/test.java
+++ src/com/meditation/test/test.java
@@ -3,7 +3,7 @@ package com.meditation.test;
 public class test {
 
     public static void main(String[] args) {
+    //git
 
-        ////////撒大苏打阿斯顿啊sssqweqweqwe哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈还好噶湖广会馆规范法官和规范
     }
 }
