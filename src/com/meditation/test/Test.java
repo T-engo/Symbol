caad77fcd4decacb3acb94cd14f7e09c517d6ee0
T-engo <1092738496@qq.com>
2020-01-18 13:50:14 +0800

    1.18 测试git远程分支传送

--- src/com/meditation/test/Test.java
+++ src/com/meditation/test/Test.java
@@ -8,7 +8,7 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        String dd = "www";
+        String dd = "wwwsssss";
         String ss = "wwww";
         ArrayList list = new ArrayList<HashMap<String,String>>();
         Map map = new HashMap<String,String>();
